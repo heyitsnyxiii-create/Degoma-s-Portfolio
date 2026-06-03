838333431bc0ca5a28a59cd12f01f6430b169ec9
heyitsnyxiii-create <heyitsnyxiii@gmail.com>
2026-06-03 21:21:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/Degoma-s-Portfolio/',
+          
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
